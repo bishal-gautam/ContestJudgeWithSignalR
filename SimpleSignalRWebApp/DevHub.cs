5ba545cd2cfa795e3ea854fb799e145b3feb53c6
Bishal <bishal@devskill.com>
2018-01-18 14:30:11 +0600

    siganlR test-1

---
+++ SimpleSignalRWebApp/DevHub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SimpleSignalRWebApp
+{
+    public class DevHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void BroadCastReply(string reply)
+        {
+            Clients.Others.QueryResponder(reply);
+        }
+
+        public void askQuery(string id,string qry)
+        {
+            Clients.Others.QueryReciever(id,qry);
+        }
+    }
+}
---
+++ SimpleSignalRWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimpleSignalRWebApp.MvcApplication" Language="C#" %>
